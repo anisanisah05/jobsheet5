dd2a47effdc7d05c049f3dd7bb8e19088a45f6df
NISAH~05 <NISAH~05@DESKTOP-KFPBT93>
2018-09-06 10:47:11 +0700

    commit keseluruhan

---
+++ src/praktikum/praktikum2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+/**
+ *
+ * @author NISAH~05
+ */
+public class praktikum2 {
+    public static void main(String args[]) {
+
+try {
+
+tesp2.myMethod();
+
+} catch (ClassNotFoundException e) {
+
+System.out.println(e);
+
+}
+
+}
+
+}
+
+
